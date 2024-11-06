a890550e34591d4a18e1cf209934cf56d4c3ea5b
RafaelSlva <168441084+RafaelSlva@users.noreply.github.com>
2024-11-06 16:30:14 -0300

    arquivos enviados

---
+++ perguntas.js
@@ -0,0 +1,89 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Games',
+    'Quando fortnite foi lançado?',
+    '2011'
+)
+
+
+criaCartao(
+    `futebol`,
+    `melhor time`,
+    `Corinthians`
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Tnoevry Grvkrven, niryvab*/
